6e45d443967c9040731ef450572de66761c16696
KeepReal <lhe@emich.edu>
2018-01-26 17:15:09 -0500

    Upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# README
+
+## Intro
+
+This python files will generate a website.
+Which is a movie relevant website includes movie title, poster and trailer. 
+Click on any of the posters will show the trailer video.
+
+## How to Run the Code
+
+1. Download the code or copy the repository.
+2. Run entertainment_center.py:
+  - **Using Terminal(Mac OS):** run Terminal -> using `cd ` go to the files' path -> run `python entertainment_center.py`
+  - **Using IDLE:** Download IDLE from [Python](https://www.python.org/) -> open entertainment_center.py in IDLE -> Menu, Run
+3. after step 2, there will be a website open in your browser as below, click any poster could show the trailer.
+  ![ScreenShot](ScreenShot.png)
+
+## License
+
+MIT
+
+[**Python Official License**](https://docs.python.org/3/license.html)
---
+++ entertainment_center.py
@@ -0,0 +1,27 @@
+import media
+import fresh_tomatoes
+
+
+# Toy Story: movie title, storyline, poster image and movie trailer
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy  and his toy that come to life",
+                        "http://www.impawards.com/1995/posters/toy_story_ver1_xlg.jpg",  # NOQA
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+# Avatar: movie title, storyline, poster image and movie trailer
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "http://4.bp.blogspot.com/_1qPLMlz01yQ/SwTA3GFVzxI/AAAAAAAADts/irJykYj7hJs/s1600/MarketSaw_03+Nov.+18+23.51.jpg",  # NOQA
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+# Man on Fire: movie title, storyline, poster image and movie trailer
+man_on_fire = media.Movie("Man on Fire",
+                          "A cold blood revenge",
+                          "https://i.pinimg.com/736x/e2/53/9a/e2539af69a7cb48e3c901a9b2392eca4--elderly-man-man-on-fire.jpg",  # NOQA
+                          "https://www.youtube.com/watch?v=rb-ZEBBKolc")
+
+# set the movies that will be passed to the media file
+movies = [toy_story, avatar, man_on_fire]
+
+# open the HTML file in a browser via the fresh_tomatoes.py file
+fresh_tomatoes.open_movies_page(movies)
